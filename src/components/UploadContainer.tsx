cb69a28e1216e1802ebeaaf9aed9710179c30704
AppleFlavored <boostelixir@gmail.com>
2023-05-05 19:39:29 -0400

    Update upload container

--- src/components/UploadContainer.tsx
+++ src/components/UploadContainer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { DragEvent, useRef } from 'react';
+import { DragEvent } from 'react';
 
 export default function UploadContainer() {
     const handleDrop = (event: DragEvent) => {
@@ -8,16 +8,23 @@ export default function UploadContainer() {
     }
 
     return (
-        <div
-            className="flex flex-col justify-center items-center border-2 border-[#272f37] border-dashed rounded h-48 select-none"
+        <form
+            className=" border-2 border-[#2a333c] border-dashed rounded h-48 select-none"
             onDrop={handleDrop}
         >
-            <h2 className="text-[#35404b] text-xl font-semibold">Drag and Drop</h2>
-            <div className="inline-flex items-center justify-center w-full">
-                <hr className="w-64 h-px my-4 bg-[#272f37] border-0" />
-                <span className="absolute px-3 text-[#35404b] -translate-x-1/2 bg-[#1b2028] left-1/2">or</span>
-            </div>
-            <button className="bg-[#272f37] text-[hsl(210,5%,50%)] rounded p-2">Select a File</button>
-        </div>
+            <input
+                type="file"
+                id="fileInput"
+                className="bg-[#272f37] text-[#798086] rounded p-2 opacity-0 w-0 h-0 absolute"
+                />
+            <label htmlFor="fileInput" className="flex flex-col justify-center items-center w-full h-full text-[#3f4d5a] ">
+                <h2 className="text-xl font-semibold">Drag and Drop</h2>
+                <div className="inline-flex items-center justify-center w-full">
+                    <hr className="w-64 h-px my-4 bg-[#272f37] border-0" />
+                    <span className="absolute px-3 -translate-x-1/2 bg-[#1b2028] left-1/2">or</span>
+                </div>
+                <p>Click to select a .log file</p>
+            </label>
+        </form>
     );
 }
