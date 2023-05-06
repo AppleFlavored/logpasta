974f7caed3e0d10f2aeb8b79ea4e402e0b14da72
AppleFlavored <boostelixir@gmail.com>
2023-05-06 16:52:36 -0400

    File uploading is done :sunglasses:

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,8 +1,24 @@
-import { NextResponse } from "next/server";
+import { prisma } from '@/server/db';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
     const formData = await request.formData();
-    console.log(formData);
+    if (!formData.has('file')) {
+        return NextResponse.json({ error: 'Invalid upload request.' }, { status: 400 });
+    }
 
-    return NextResponse.json({});
+    const file = formData.get('file') as File;
+    if (file.size > 5e+6) {
+        // Max file size: 5 MB (or 5e+6 bytes)
+        return NextResponse.json({ error: 'File upload is too big!' }, { status: 400 });
+    }
+
+    const source = await file.text();
+    const document = await prisma.textDocument.create({
+        data: { source },
+    });
+
+    return NextResponse.json({
+        uploadId: document.id
+    }, { status: 200 });
 }
--- src/components/UploadContainer.tsx
+++ src/components/UploadContainer.tsx
@@ -1,43 +1,43 @@
 'use client';
 
-import { DragEvent, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import Dropzone from 'react-dropzone';
 
 export default function UploadContainer() {
-    const filesRef = useRef<FileList>();
-    
-    const handleDrop = (event: DragEvent) => {
-        event.preventDefault();
+    const router = useRouter();
 
-        const files = event.dataTransfer.files;
-        filesRef.current = files;
-        for (let i = 0; i < files.length; i++) {
-            console.log(files.item(i)?.type);
+    const handleDrop = async (acceptedFiles: File[]) => {
+        if (acceptedFiles.length === 0) {
+            throw new Error('File upload error.');
         }
 
-        handleChange();
-    }
+        const formData = new FormData();
+        formData.append('file', acceptedFiles[0], acceptedFiles[0].name);
 
-    const handleChange = () => {
-        alert('upload');
+        const res = await fetch('/api/upload', { method: 'POST', body: formData })
+            .then(res => res.json())
+            .catch(_ => {
+                throw new Error('File upload failed.');
+            });
+
+        router.push(`/${res.uploadId}`)
     }
 
     return (
-        <form onDrop={handleDrop} className="border-2 border-[#2a333c] border-dashed rounded h-48 select-none">
-            <input
-                type="file"
-                id="fileInput"
-                accept="text/*"
-                className="bg-[#272f37] text-[#798086] rounded p-2 opacity-0 w-0 h-0 absolute"
-                onChange={handleChange}
-                />
-            <label htmlFor="fileInput" className="flex flex-col justify-center items-center w-full h-full text-[#3f4d5a]">
-                <h2 className="text-xl font-semibold">Drag and Drop</h2>
-                <div className="inline-flex items-center justify-center w-full">
-                    <hr className="w-64 h-px my-4 bg-[#272f37] border-0" />
-                    <span className="absolute px-3 -translate-x-1/2 bg-[#1b2028] left-1/2">or</span>
+        <Dropzone onDrop={handleDrop} maxFiles={1} maxSize={5e+6}>
+            {({getRootProps, getInputProps}) => (
+                <div className="border-2 border-[#2a333c] border-dashed rounded h-48 select-none" {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <div className="flex flex-col justify-center items-center w-full h-full text-[#3f4d5a]">
+                        <h2 className="text-xl font-semibold">Drag and Drop</h2>
+                        <div className="inline-flex items-center justify-center w-full">
+                            <hr className="w-64 h-px my-4 bg-[#272f37] border-0" />
+                            <span className="absolute px-3 -translate-x-1/2 bg-[#1b2028] left-1/2">or</span>
+                        </div>
+                        <p>Click to select a .log file</p>
+                    </div>
                 </div>
-                <p>Click to select a .log file</p>
-            </label>
-        </form>
+            )}
+        </Dropzone>
     );
 }
