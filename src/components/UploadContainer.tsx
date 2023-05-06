766a1b4bfc910476878aa8a91434804d212b6e7d
AppleFlavored <boostelixir@gmail.com>
2023-05-06 19:00:14 -0400

    Add user-facing error messages to upload step

--- src/components/UploadContainer.tsx
+++ src/components/UploadContainer.tsx
@@ -4,14 +4,19 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import Dropzone from 'react-dropzone';
 import { ImSpinner2 } from 'react-icons/im';
+import { MdError } from 'react-icons/md';
 
 export default function UploadContainer() {
     const router = useRouter();
     const [isLoading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
     const handleDrop = async (acceptedFiles: File[]) => {
+        setError(null);
+
         if (acceptedFiles.length === 0) {
-            throw new Error('File upload error.');
+            setError('You can only upload one file at a time! (or the file is larger than 5 MB)');
+            return;
         }
 
         const formData = new FormData();
@@ -20,11 +25,15 @@ export default function UploadContainer() {
         setLoading(true);
         const res = await fetch('/api/upload', { method: 'POST', body: formData })
             .then(res => res.json())
-            .catch(_ => {
+            .catch(err => {
+                console.log(err);
                 setLoading(false);
-                throw new Error('File upload failed.');
+                setError('Invalid file format.');
             });
 
+        if (!res) {
+            return;
+        }
         router.push(`/${res.uploadId}`)
     }
 
@@ -34,22 +43,33 @@ export default function UploadContainer() {
                 <div className="flex items-center justify-center border-2 border-[#3571fe] border-dashed rounded h-48">
                     <ImSpinner2 className="w-8 h-8 fill-[#3571fe] animate-spin" />
                 </div>
-            :
-                <Dropzone onDrop={handleDrop} maxFiles={1} maxSize={5e+6}>
-                    {({getRootProps, getInputProps}) => (
-                        <div className="border-2 border-[#2a333c] border-dashed rounded h-48 select-none" {...getRootProps()}>
-                            <input {...getInputProps()} />
-                            <div className="flex flex-col justify-center items-center w-full h-full text-[#3f4d5a]">
-                                <h2 className="text-xl font-semibold">Drag and Drop</h2>
-                                <div className="inline-flex items-center justify-center w-full">
-                                    <hr className="w-64 h-px my-4 bg-[#272f37] border-0" />
-                                    <span className="absolute px-3 -translate-x-1/2 bg-[#1b2028] left-1/2">or</span>
-                                </div>
-                                <p>Click to select a .log file</p>
+            : 
+                <>
+                    { error &&
+                        <div className="flex items-center rounded border border-red-400 gap-4 p-4 mb-8">
+                            <MdError className="w-10 h-10 fill-red-400" />
+                            <div className="flex-grow">
+                                <p className='font-semibold text-red-400'>An error occurred while uploading!</p>
+                                <p>{error}</p>
                             </div>
                         </div>
-                    )}
-                </Dropzone>
+                    }
+                    <Dropzone onDrop={handleDrop} maxFiles={1} maxSize={5e+6}>
+                        {({getRootProps, getInputProps}) => (
+                            <div className="border-2 border-[#2a333c] border-dashed rounded h-48 select-none" {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                <div className="flex flex-col justify-center items-center w-full h-full text-[#3f4d5a]">
+                                    <h2 className="text-xl font-semibold">Drag and Drop</h2>
+                                    <div className="inline-flex items-center justify-center w-full">
+                                        <hr className="w-64 h-px my-4 bg-[#272f37] border-0" />
+                                        <span className="absolute px-3 -translate-x-1/2 bg-[#1b2028] left-1/2">or</span>
+                                    </div>
+                                    <p>Click to select a .log file</p>
+                                </div>
+                            </div>
+                        )}
+                    </Dropzone>
+                </>
             }
         </>
     );
