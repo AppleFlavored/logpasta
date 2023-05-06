3c48106cea80b880ff77301ffa2d3c3d3b0ebad1
AppleFlavored <boostelixir@gmail.com>
2023-05-06 18:03:26 -0400

    Update information on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,14 @@ export default function Home() {
                 <UploadContainer />
                 <p className="text-center text-[#3f4d5a] text-sm mt-2">(Max file size: 5 MB)</p>
             </div>
-            <div className="container mx-auto grid grid-cols-3 gap-16 max-w-4xl mt-16">
+            <div className="container mx-auto grid grid-cols-2 gap-16 max-w-4xl mt-16">
                 <div className="text-center">
                     <h1 className="font-bold text-2xl flex items-center justify-center gap-x-2 mb-2"><MdShare />Share</h1>
-                    <p>Let others view your Minecraft logs with the click of a link.</p>
+                    <p>Let others view your Minecraft logs with the click of a link. Private information like IP addresses is censored.</p>
                 </div>
                 <div className="text-center">
                     <h1 className="font-bold text-2xl flex items-center justify-center gap-x-2 mb-2"><MdTroubleshoot />Troubleshoot</h1>
                     <p>LogPasta will automatically scan your logs and give solutions to known problems.</p>
-                </div>
-                <div className="text-center">
-                    <h1 className="font-bold text-2xl flex items-center justify-center gap-x-2 mb-2"><MdLock />Stay Private</h1>
-                    <p>IP addresses, usernames, and UUIDs will be removed the moment you upload your logs.</p>
                 </div>
             </div>
         </>
