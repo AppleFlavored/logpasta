9518f0d474427bf71bf8f9a16b03e964cabb8577
AppleFlavored <boostelixir@gmail.com>
2023-05-29 21:04:10 -0400

    Add padding for mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import UploadContainer from '@/components/UploadContainer';
-import { MdShare, MdTroubleshoot, MdLock } from 'react-icons/md';
+import { MdShare, MdTroubleshoot } from 'react-icons/md';
 
 export default function Home() {
     return (
         <>
-            <div className="container mx-auto max-w-3xl mt-16">
+            <div className="container mx-auto px-2 sm:p-0 max-w-3xl mt-16">
                 <UploadContainer />
                 <p className="text-center text-[#3f4d5a] text-sm mt-2">(Max file size: 5 MB)</p>
             </div>
-            <div className="container mx-auto grid grid-cols-2 gap-16 max-w-4xl mt-16">
+            <div className="container mx-auto px-2 sm:p-0 grid grid-rows-2 sm:grid-cols-2 gap-16 max-w-4xl mt-16">
                 <div className="text-center">
                     <h1 className="font-bold text-2xl flex items-center justify-center gap-x-2 mb-2"><MdShare />Share</h1>
                     <p>Let others view your Minecraft logs with the click of a link. Private information like IP addresses is censored.</p>
